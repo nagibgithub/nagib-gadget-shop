3bb2f3f9905fc9e8560c3c1a871706a5f22e310e
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-04-09 11:54:26 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+    {
+
+    }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
